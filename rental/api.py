37ae937d4ec770e13c11dffcd0197c05add2ea99
Dominik Kozaczko <dominik@kozaczko.info>
2019-04-25 13:19:04 +0200

    Part 06 - Nesting

--- rental/api.py
+++ rental/api.py
@@ -1,7 +1,20 @@
 from rest_framework import routers
+from rest_framework_extensions.routers import NestedRouterMixin
+
 from core import views as myapp_views
 
-router = routers.DefaultRouter()
-router.register(r'friends', myapp_views.FriendViewset)
+
+class NestedDefaultRouter(NestedRouterMixin, routers.DefaultRouter):
+    pass
+
+
+router = NestedDefaultRouter()
+friends = router.register(r'friends', myapp_views.FriendViewset)
+friends.register(
+    r'borrowings',
+    myapp_views.BorrowedViewset,
+    base_name='friend-borrow',
+    parents_query_lookups=['to_who']
+)
 router.register(r'belongings', myapp_views.BelongingViewset)
 router.register(r'borrowings', myapp_views.BorrowedViewset)
